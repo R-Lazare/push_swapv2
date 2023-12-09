e44b07a7747192f8c9806f2bf0a5e6f18dc48ee0
rluiz <rluiz@ubuntu.myguest.virtualbox.org>
2023-12-09 16:23:54 +0100

    thing

--- pushswap.c
+++ pushswap.c
@@ -6,7 +6,7 @@
 /*   By: rluiz <rluiz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 15:17:07 by rluiz             #+#    #+#             */
-/*   Updated: 2023/12/09 15:45:41 by rluiz            ###   ########.fr       */
+/*   Updated: 2023/12/09 16:23:47 by rluiz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -141,6 +141,26 @@ static t_pile	*find_highest(t_pile *stack)
 	return (highest_node);
 }
 
+static t_pile	*find_smallest(t_pile *stack)
+{
+	int				smallest;
+	t_pile	*smallest_node;
+
+	if (NULL == stack)
+		return (NULL);
+	smallest = INT_MAX;
+	while (stack)
+	{
+		if (stack->value < smallest)
+		{
+			smallest = stack->value;
+			smallest_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (smallest_node);
+}
+
 void	sort_three(t_pile **pile_a)
 {
 	t_pile	*highest_node;
@@ -167,6 +187,46 @@ int	is_sorted(t_pile *stack)
 	return (1);
 }
 
+static void	set_target_node(t_pile *a, t_pile *b)
+{
+	t_pile	*current_a;
+	t_pile	*target_node;
+	long			best_match_index;
+
+	while (b)
+	{
+		best_match_index = LONG_MAX;
+		current_a = a;
+		while (current_a)
+		{
+			if (current_a->value > b->value
+				&& current_a->value < best_match_index)
+			{
+				best_match_index = current_a->value;
+				target_node = current_a;
+			}
+			current_a = current_a->next;
+		}
+		if (LONG_MAX == best_match_index)
+			b->target = find_smallest(a);
+		else
+			b->target = target_node;
+		b = b->next;
+	}
+}
+
+void	push_all_but_three(t_data *data)
+{
+	int		i;
+
+	i = 0;
+	while (i < data->total_len - 3)
+	{
+		pb(data);
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	t_arena	*arena;
@@ -182,6 +242,8 @@ int	main(int argc, char **argv)
 	print_piles(data);
 	if (data->total_len == 3)
 		sort_three(&(data->pile_a));
+	push_all_but_three(data);
+	set_target_node(data->pile_a, data->pile_b);
 	printf("After sorting:\n");
 	print_piles(data);
 	arena_destroy(arena);
