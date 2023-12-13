a72ba2c2628326df4db88086f7b988f146ffbda5
R-Lazare <84543755+R-Lazare@users.noreply.github.com>
2023-12-13 18:24:31 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -12,66 +12,66 @@
 
 #include "pushswap.h"
 
-// static char	*allocate_and_copy(const char *src, const char *buff,
-// 		t_arena *arena)
-// {
-// 	char	*dest;
-// 	int		length;
-// 	int 	i;
+static char	*allocate_and_copy(const char *src, const char *buff,
+		t_arena *arena)
+{
+	char	*dest;
+	int		length;
+	int 	i;
 
-// 	length = ft_strlen_line(buff) + 1;
-// 	if (src)
-// 	{
-// 		length += ft_strlen(src);
-// 	}
-// 	dest = arena_alloc(arena, sizeof(char) * length + 1);
-// 	if (!dest)
-// 	{
-// 		return (NULL);
-// 	}
-// 	i = 0;
-// 	if (src)
-// 	{
-// 		while (src[i])
-// 		{
-// 			dest[i] = src[i];
-// 			i++;
-// 		}
-// 	}
-// 	return (dest);
-// }
+	length = ft_strlen_line(buff) + 1;
+	if (src)
+	{
+		length += ft_strlen(src);
+	}
+	dest = arena_alloc(arena, sizeof(char) * length + 1);
+	if (!dest)
+	{
+		return (NULL);
+	}
+	i = 0;
+	if (src)
+	{
+		while (src[i])
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	}
+	return (dest);
+}
 
-// static char	*concatenate_line(char *dest, const char *buff)
-// {
-// 	int	i;
-// 	int	j;
+static char	*concatenate_line(char *dest, const char *buff)
+{
+	int	i;
+	int	j;
 
-// 	if (dest)
-// 		i = ft_strlen(dest);
-// 	else
-// 		i = 0;
-// 	j = 0;
-// 	while (buff[j] && buff[j] != '\n')
-// 	{
-// 		dest[i++] = buff[j++];
-// 	}
-// 	dest[i++] = '\n';
-// 	dest[i] = '\0';
-// 	if (dest[0] == '\0')
-// 	{
-// 		return (NULL);
-// 	}
-// 	return (dest);
-// }
+	if (dest)
+		i = ft_strlen(dest);
+	else
+		i = 0;
+	j = 0;
+	while (buff[j] && buff[j] != '\n')
+	{
+		dest[i++] = buff[j++];
+	}
+	dest[i++] = '\n';
+	dest[i] = '\0';
+	if (dest[0] == '\0')
+	{
+		return (NULL);
+	}
+	return (dest);
+}
 
-// char	*ft_join_line(char *src, char *buff, t_arena *arena)
-// {
-// 	char	*dest;
+char	*ft_join_line(char *src, char *buff, t_arena *arena)
+{
+	char	*dest;
 
-// 	dest = allocate_and_copy(src, buff, arena);
-// 	if (!dest)
-// 	{
-// 		return (NULL);
-// 	}
-// 	return (concatenate_line(dest, buff));
-// }
+	dest = allocate_and_copy(src, buff, arena);
+	if (!dest)
+	{
+		return (NULL);
+	}
+	return (concatenate_line(dest, buff));
+}
