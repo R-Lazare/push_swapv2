a07794ea3b9020e12ed81021ca84bf6d310523f9
R-Lazare <84543755+R-Lazare@users.noreply.github.com>
2023-12-13 18:24:51 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -38,40 +38,40 @@ int	ft_strlen_line(const char *dest)
 	return (i);
 }
 
-char	*ft_join_line(char *src, char *buff, t_arena *arena)
-{
-	int		i;
-	int		j;
-	char	*dest;
+// char	*ft_join_line(char *src, char *buff, t_arena *arena)
+// {
+// 	int		i;
+// 	int		j;
+// 	char	*dest;
 
-	i = 0;
-	j = 0;
-	if (!src)
-		dest = arena_alloc(arena, sizeof(char) * ft_strlen_line(buff) + 2);
-	else
-	{
-		dest = arena_alloc(arena, sizeof(char) * ft_strlen_line(buff)
-			+ ft_strlen(src) + 2);
-		while (src[i])
-		{
-			dest[i] = src[i];
-			i++;
-		}
-	}
-	while (buff[j] && buff[j] != '\n')
-	{
-		dest[i] = buff[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\n';
-	if (buff[j] == '\n')
-		i++;
-	dest[i] = '\0';
-	if (dest[0] == '\0')
-		return (NULL);
-	return (dest);
-}
+// 	i = 0;
+// 	j = 0;
+// 	if (!src)
+// 		dest = arena_alloc(arena, sizeof(char) * ft_strlen_line(buff) + 2);
+// 	else
+// 	{
+// 		dest = arena_alloc(arena, sizeof(char) * ft_strlen_line(buff)
+// 			+ ft_strlen(src) + 2);
+// 		while (src[i])
+// 		{
+// 			dest[i] = src[i];
+// 			i++;
+// 		}
+// 	}
+// 	while (buff[j] && buff[j] != '\n')
+// 	{
+// 		dest[i] = buff[j];
+// 		i++;
+// 		j++;
+// 	}
+// 	dest[i] = '\n';
+// 	if (buff[j] == '\n')
+// 		i++;
+// 	dest[i] = '\0';
+// 	if (dest[0] == '\0')
+// 		return (NULL);
+// 	return (dest);
+// }
 
 void	ft_cute_line(char *dest)
 {
